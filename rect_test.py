ac9ebe3341ba17188fd29d53a775da7751568329
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2020-10-12 09:47:33 +0200

    Add unit tests

--- rect_test.py
+++ rect_test.py
@@ -5,7 +5,7 @@ import rectangles as r
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_background_top(self):
+    def test_background_corners(self):
         B = r.RectangleLayout(Fraction(0, 1), Fraction(0, 1),
                               Fraction(1, 1), Fraction(1, 1))
         R = []
@@ -24,6 +24,14 @@ class TestStringMethods(unittest.TestCase):
         x, y = r.get_topleft_corner(B, R)
         self.assertEqual(x, 0)
         self.assertEqual(y, Fraction(1, 2))
+        B.top = [r.RectangleLayout(Fraction(0, 1), Fraction(0, 1),
+                                   Fraction(1, 1), Fraction(1, 1))]
+        B.left = B.top
+        B.rigth = B.top
+        B.bottom = B.top
+        x, y = r.get_topleft_corner(B, R)
+        self.assertEqual(x, -1)
+        self.assertEqual(y, -1)
 
 
 if __name__ == '__main__':
